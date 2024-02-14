dcd9b3321ced0c12456e607046ca9290ff4ad692
Mohsin <55811423+mosin92@users.noreply.github.com>
2024-02-14 12:24:35 +0500

    initial commit

---
+++ src/SongsLists/Songs.js
@@ -0,0 +1,146 @@
+export const songsList = [
+  {
+    title: 'Mesos de l’any',
+    artist: 'Assumpta Rojas Gibert',
+    artwork: require('../images/Dite2_foto.jpg'),
+    url: require('../audios/Dites2.mp3'),
+    id: '1',
+  },
+  {
+    title: 'A l’estiu Tota cuca viu',
+    artist: 'Assumpta Rojas Gibert',
+    artwork: require('../images/Dite3_foto.jpg'),
+    url: require('../audios/Dites3.mp3'),
+    id: '2',
+  },
+  {
+    title: 'Qui dia passa, any empeny',
+    artist: 'Assumpta Rojas Gibert',
+    artwork: require('../images/Dites1_foto.jpg'),
+    url: require('../audios/Dites1.mp3'),
+    id: '3',
+  },
+  {
+    title: 'El Cant dels ocells',
+    artist: 'Flàvia Camacho Martínez',
+    artwork: require('../images/CantdelsOcells.jpeg'),
+    url: require('../audios/Cant_dels_ocells.mp3'),
+    id: '4',
+  },
+  {
+    title: 'El noi de la mare',
+    artist: 'Cristina Ferrero Celaya',
+    artwork: require('../images/Noidelamare.webp'),
+    url: require('../audios/El_noi_de_la_mare.mp3'),
+    id: '5',
+  },
+  {
+    title: 'La lluna la pruna',
+    artist: 'Cristina Ferrero Celaya',
+    artwork: require('../images/Lallunalapruna.webp'),
+    url: require('../audios/La_lluna_la_pruna.mp3'),
+    id: '6',
+  },
+  {
+    title: 'Les Trementinaires', 
+    artist: '✍️Pilar Pelegri Terrado',
+    flag: '333',
+    artwork: require('../images/foto-Trementinaire.jpg'),
+    url: require('../audios/Les_Trementinaires.mp3'),
+    id: '7',
+  },
+
+  {
+    title: 'Las Trementinaires',
+    artist: '✍️Pilar Pelegri Terrado',
+    flag: '777',
+    artwork: require('../images/foto-Trementinaire.jpg'),
+    url: require('../audios/Cast_LesTrementinaires.mp3'),
+    id: '8',
+  },
+
+  {
+    title: "L'arribada del tren a Mataró",
+    artist: 'KPimarketing.es',
+    flag: '333',
+    artwork: require('../images/Mataro_foto.jpg'),
+    url: require('../audios/TrenMataro.mp3'),
+    id: '9',
+  },
+
+  {
+    title: "La llegada del tren a Mataró",
+    artist: 'KPimarketing.es',
+    flag: '777',
+    artwork: require('../images/Mataro_foto.jpg'),
+    url: require('../audios/Cast_TrenMataro.mp3'),
+    id: '10',
+  },
+
+  {
+    title: 'El Timbaler del Bruc',
+    artist: 'Català',
+    flag: '333',
+    artwork: require('../images/timbaler4.jpg'),
+    url: require('../audios/Timbaler.mp3'),
+    id: '11',
+  },
+
+  {
+    title: 'El Tamborilero del Bruc',
+    artist: 'Castellano',
+    flag: '777',
+    artwork: require('../images/timbaler4.jpg'),
+    url: require('../audios/Cast_LeyendaTimbaler.mp3'),
+    id: '12',
+  },
+
+  {
+    title: 'Manresa el misteri de la llum',
+    artist: 'Alba Subirà Fernández',
+    flag: '333',
+    artwork: require('../images/Manresa_foto_new.jpeg'),
+    url: require('../audios/Manresa.mp3'),
+    id: '13',
+  },
+  {
+    title: 'Medistoris',
+    artist: 'Aplicació',
+    artwork: require('../images/welcome.webp'),
+    url: require('../audios/welcome.mp3'),
+    id: '14',
+  },
+];
+
+export const sliderSongs = [
+  // {
+  //   title: 'Manresa el misteri de la llum',
+  //   artist: 'Alba Subirà Fernández',
+  //   artwork: require('../images/Manresa_foto.jpg'),
+  //   url: require('../audios/Manresa.mp3'),
+  //   id: '10',
+  // },
+
+  {
+    title: 'El Cant dels ocells',
+    artist: 'Flàvia Camacho Martínez',
+    artwork: require('../images/CantdelsOcells.jpeg'),
+    url: require('../audios/Cant_dels_ocells.mp3'),
+    id: '4',
+  },
+  {
+    title: "L'arribada del tren a Mataró",
+    artist: 'KPimarketing.es',
+    flag: '333',
+    artwork: require('../images/Mataro_foto.jpg'),
+    url: require('../audios/TrenMataro.mp3'),
+    id: '9',
+  },
+  {
+    title: 'Medistoris',
+    artist: 'Aplicació',
+    artwork: require('../images/welcome.webp'),
+    url: require('../audios/welcome.mp3'),
+    id: '14',
+  },
+];
---
+++ src/SongsLists/SongsLlegenda.js
@@ -0,0 +1,28 @@
+export const songsList = [
+  {
+    title: 'El Timbaler del Bruc',
+    artist: 'Català',
+    flag: '333',
+    artwork: require('../images/timbaler4.jpg'),
+    url: require('../audios/Timbaler.mp3'),
+    id: '11',
+  },
+
+  {
+    title: 'El Tamborilero del Bruc',
+    artist: 'Castellano',
+    flag: '777',
+    artwork: require('../images/timbaler4.jpg'),
+    url: require('../audios/Cast_LeyendaTimbaler.mp3'),
+    id: '12',
+  },
+
+  {
+    title: 'Manresa el misteri de la llum',
+    artist: 'Alba Subirà Fernández',
+    flag: '333',
+    artwork: require('../images/Manresa_foto_new.jpeg'),
+    url: require('../audios/Manresa.mp3'),
+    id: '13',
+  },
+];
